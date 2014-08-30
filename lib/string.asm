1306dd4752523772411c136c1b7bf1fa34ee7f25
Yugnaynehc <cyyyyc123@hotmail.com>
2014-08-30 13:35:18 +0800

    add function:memset

--- lib/string.asm
+++ lib/string.asm
@@ -9,7 +9,7 @@
 
 ;;; export functions
 global memcpy    
-
+global memset
 
 
 
@@ -48,3 +48,38 @@ memcpy:
 
     ret
     
+
+
+
+
+
+;;; void memset(void* p_dst, char ch, int size);
+memset:
+    push ebp
+    mov  ebp, esp
+
+    push esi
+    push edi
+    push ecx
+
+    mov  edi, [ebp + 8]         ; edi = p_dst
+    mov  esi, [ebp + 12]        ; dsi = p_src
+    mov  ecx, [ebp + 16]        ; ecx = size
+.1:
+    cmp  ecx, 0
+    jz   .2
+
+    mov  byte [edi], dl
+    inc  edi
+
+    dec  ecx
+    jmp  .1
+.2:
+
+    pop  ecx
+    pop  edi
+    pop  esi
+    mov  esp, ebp
+    pop  ebp
+
+    ret
